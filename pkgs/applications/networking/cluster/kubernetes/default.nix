9b224e5e3a83050da617f82deda28e50320526f3
Stefan Lau <github@stefanlau.com>
2017-05-18 23:15:54 +0200

    kubernetes: 1.5.6 -> 1.6.2

--- pkgs/applications/networking/cluster/kubernetes/default.nix
+++ pkgs/applications/networking/cluster/kubernetes/default.nix
@@ -8,7 +8,6 @@
     "cmd/kube-controller-manager"
     "cmd/kube-proxy"
     "plugin/cmd/kube-scheduler"
-    "cmd/kube-dns"
     "federation/cmd/federation-apiserver"
     "federation/cmd/federation-controller-manager"
   ]
@@ -18,13 +17,13 @@ with lib;
 
 stdenv.mkDerivation rec {
   name = "kubernetes-${version}";
-  version = "1.5.6";
+  version = "1.6.2";
 
   src = fetchFromGitHub {
     owner = "kubernetes";
     repo = "kubernetes";
     rev = "v${version}";
-    sha256 = "0mkg4vgz9szgq1k5ignkdr5gmg703xlq8zsrr422a1qfqb8zp15w";
+    sha256 = "1bvcgd8r41gr3ca6x02xv121mj6vac7vx2h0sd45kbjn7jmwbcjf";
   };
 
   buildInputs = [ removeReferencesTo makeWrapper which go rsync go-bindata ];
